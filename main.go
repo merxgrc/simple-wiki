b9630a9a2fffac9206b006fd4a167e538c8e6d20
merxgrc <mgarcia73@mail.sfsu.edu>
2025-05-03 20:12:07 -0700

    able to load and save pages and print their content to the console.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
@@ -9,17 +10,27 @@ type Page struct {
 	Body  []byte //slice
 }
 
-// This method takes pointer to a a page a page, it takes no parameters and returns an error.
-// It saves the page's body to a text file. It returns an error because that is the return type of WriteFile
+// This is a method it takes as it's receiver a pointer to a page, it takes no parameters and returns an error.
+// It saves the page's body to a text file. It returns an error because that is the return type of WriteFile.
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
 }
 
-func main() {
-	p := &Page{Title: "Hydrogen", Body: []byte("H is the first element.")}
-	err := p.save()
+// This is a function it loads a page, Readfile returns either a byte slice or an error.
+// We are returning an error if it fails or a pointer to the new page struct by reference.
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
+	return &Page{Body: body, Title: title}, nil
+}
+
+func main() {
+	p1 := &Page{Title: "Sample Page", Body: []byte("This is a sample page.")}
+	p1.save()
+	p2, _ := loadPage("Sample Page")
+	fmt.Println(string(p2.Title) + ": " + string(p2.Body))
 }
