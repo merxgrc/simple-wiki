58d6870e871586d98bf832b4a1407b0666cd9d99
merxgrc <mgarcia73@mail.sfsu.edu>
2025-05-05 13:08:50 -0700

    added comments to server.go

--- server/server.go
+++ server/server.go
@@ -7,10 +7,11 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[0:])
 }
 
+// Main function sets up an http server that listens on port 8080 and uses the handler function for all routes.
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler) // handler is just the name I decided to give the function it can be anything.
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
