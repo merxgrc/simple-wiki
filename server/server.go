e5d235ce48982e30002f4f323c2b500bf761f5b2
merxgrc <mgarcia73@mail.sfsu.edu>
2025-05-05 13:28:34 -0700

    added comments explaining what the code does

--- server/server.go
+++ server/server.go
@@ -6,12 +6,13 @@ import (
 	"net/http"
 )
 
+// This function handles http requests.
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[0:])
 }
 
 // Main function sets up an http server that listens on port 8080 and uses the handler function for all routes.
 func main() {
-	http.HandleFunc("/", handler) // handler is just the name I decided to give the function it can be anything.
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", handler)                // Registers the handler function when a request matches the "/" pattern
+	log.Fatal(http.ListenAndServe(":8080", nil)) // Start an http web server on port 8080, crash if error.
 }
